75517a0e35d5e5d99101ac54a0b89f3594a52909
Zahraa Ashraf <104033819+Zahraa5Ashraf@users.noreply.github.com>
2023-01-07 22:09:41 +0200

    Update VanyaFence.py

--- VanyaFence.py
+++ VanyaFence.py
@@ -1,8 +1,8 @@
 # user enter values
-n= int(input("text")) 
-h= int(input("text"))
+n= int(input("Enter number of friends")) 
+h= int(input("Enter Height of fence"))
 #enter from user into list
-friends= list(map(int, input("text").split()))
+friends= list(map(int, input("Enter heights of friends").split()))
 w=0  #define width variable
 for i in friends:
 	if i>h : w+=2
